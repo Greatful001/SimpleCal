8d25e0079b29890d05c9cbaf7b36a8d2a32189fd
Greatful001 <111737576+Greatful001@users.noreply.github.com>
2022-08-31 10:19:41 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+
+let buttons = document.querySelectorAll('.btn');
+let screen = document.querySelector('.screen');
+let equal = document.querySelector('.equal-btn');
+let clear = document.querySelector('.clear-btn');
+let del = document.querySelector('.del-btn');
+
+buttons.forEach(function(button) {
+    button.addEventListener('click', function(e){
+    let number = e.target.value;
+    screen.value += number;
+
+    })
+});
+
+
+equal.addEventListener('click', function (e) {
+    if (screen.value === '') {
+        alert("Enter any value to calculate");
+    }else{
+        screen.value = eval(screen.value);
+   
+    }
+    
+    });
+
+
+    clear.addEventListener('click', function (e) {
+        screen.value = "";
+    });
+    
+
+    del.addEventListener('click',function (e) {
+        screen.value -= number
+    })
